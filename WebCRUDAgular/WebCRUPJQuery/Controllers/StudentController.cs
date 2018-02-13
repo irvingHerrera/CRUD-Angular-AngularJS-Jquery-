d25e926b8fc6a722cd0de1a8a25de08285bc01e0
Irving Herrera <ih@intellekt.mx>
2018-02-13 00:55:14 -0600

    cambiios

--- WebCRUDAgular/WebCRUPJQuery/Controllers/StudentController.cs
+++ WebCRUDAgular/WebCRUPJQuery/Controllers/StudentController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 using WebCRUD.Bussines.Interfaces;
+using WebCRUD.Core.ViewModels;
 using WebCRUD.Data.RepositoryContract;
 
 namespace WebCRUPJQuery.Controllers
@@ -28,6 +29,39 @@ namespace WebCRUPJQuery.Controllers
         {
             var list = await studentBussines.GetAllStudent();
             return Json(list, JsonRequestBehavior.AllowGet);
+        }
+
+        public async Task<ActionResult> _Table()
+        {
+            var list = await studentBussines.GetAllStudent();
+            return PartialView(list);
+        }
+
+        [HttpPost]
+        public async Task<JsonResult> Delete(int id)
+        {
+            var result = await studentBussines.Delete(id);
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public async Task<JsonResult> Update(StudentViewModel student)
+        {
+            var result = await studentBussines.Update(student);
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public async Task<JsonResult> AddStudent(StudentViewModel student)
+        {
+            var result = await studentBussines.AddStudent(student);
+
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+
+        public async Task<ActionResult> _Form()
+        {
+            return View();
         }
     }
 }
